8d0eef9497e3bab8fec3c4dbe85ab35de1496696
frank <frank@bipal.com>
2024-10-26 22:28:43 +0800

    update .gitignore

--- dist/tsc/keypairs/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { Ed25519Keypair, Ed25519KeypairData, DEFAULT_ED25519_DERIVATION_PATH_SUI, } from "./keypairs";
--- dist/tsc/keypairs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_ED25519_DERIVATION_PATH_SUI = exports.Ed25519Keypair = void 0;
-var keypairs_1 = require("./keypairs");
-Object.defineProperty(exports, "Ed25519Keypair", { enumerable: true, get: function () { return keypairs_1.Ed25519Keypair; } });
-Object.defineProperty(exports, "DEFAULT_ED25519_DERIVATION_PATH_SUI", { enumerable: true, get: function () { return keypairs_1.DEFAULT_ED25519_DERIVATION_PATH_SUI; } });
-//# sourceMappingURL=index.js.map
